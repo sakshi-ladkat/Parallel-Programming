a4712c70c443b3cd1e507108856390c5d4818495
sakshi-ladkat <ladkatsakshi2507@gmail.com>
2025-09-11 23:50:59 +0530

    Parallel Assignment1

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<mpi.h>
-#include<vector>
+using namespace std;
 int main(int argc,char *argv[])
 {
    
@@ -9,68 +9,44 @@ int main(int argc,char *argv[])
    int rank,size;
    MPI_Comm_size(MPI_COMM_WORLD, &size);
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-   //std::cout<<" My rank "<<rank<<"  Out of "<<size<<std::endl;
-   int n=100;
-   int part =n/size;
-   int remainder = n%size;
-   if(rank==0)
-   {   int localsum=0;
-      std::vector<int>A;
-      for(int i=0;i<n;i++)
-       { A.push_back(i+1);}
-      int current_offset = 0;
-       // Calculating local sum on Array having rank 0
-       for(int i=0;i<part;i++)
-       { localsum+=A[i];}
-    
-       // Printing localsum on processor 0
-       std::cout<<rank<<" "<<localsum<<std::endl;
-      for(int i=1;i<size;++i)
-      { int send_count;
-	if( i < remainder)
-         { send_count = (part+1);
-	   current_offset = part+1;
-	 }
-	else
-         { send_count = part; 
-	   current_offset += part;
-	 }   
-
-      // Send data to process 'i' starting from the calculated offset
-      MPI_Send(A.data()+current_offset,send_count,MPI_INT,i,1,MPI_COMM_WORLD);       
-      }
-
-      for(int i=1;i<size;++i)
-      {
-       int sum1;
-       MPI_Recv(&sum1,1,MPI_INT,i,2,MPI_COMM_WORLD,&status);
-       std::cout<<rank<<" Localsum received from "<<i<<" Processor = "<<sum1<<std::endl;
-       int totalsum=localsum+sum1;
-       std::cout<<rank<<" Totalsum ="<<totalsum<<std::endl;
-   }
-      
-  }
-   if(rank!=0)
+   int n;
+   if(rank == 0)
    {
-     int localsum=0;
-   std::vector<int>B(part);
-      MPI_Recv(B.data(),part,MPI_INT,0,1,MPI_COMM_WORLD,&status);
-      for(int i=0;i<part;i++)
-        {    B.push_back(i);
-        }
-       /* Checking Whether Data received by B or Not 
-        for(int i=0;i<part;i++)
-        {
-           std::cout<<B[i]<<std::endl;
-        }
-        */
-         for(int i=0;i<part;i++)
-       { 
-       localsum+=B[i];
-       }
-       //Printing localsum on each processor 
-       std::cout<<rank<<" Localsum send "<<localsum<<std::endl;
-       MPI_Send(&localsum,1,MPI_INT,0,2,MPI_COMM_WORLD);
-       }
+     cout<<"Enter value of n"<<endl;
+     cin>>n;
+     for(int i=1;i<size;i++)
+     {
+     MPI_Send(&n,1,MPI_INT,i,1,MPI_COMM_WORLD);
+     }
+   }
+   if(rank !=0)
+   {
+      MPI_Recv(&n,1,MPI_INT,1,1,MPI_COMM_WORLD,status);
+   }
+   int part = n/size;
+   int remainder = n%size;
+   if(rank < remainder)
+      part = (part+1);
+   int start = (rank*part)+min(rank,remainder)+1;
+   int end   = start+part-1;
+   for(int i=start;i<=end;++i)
+   { int localsum += i; }
+   cout<<"Localsum on processor "<<rank<<"="<<localsum<<endl;
+   if(rank !=0)
+   {
+      MPI_send(&localsum,1,MPI_INT,0,2,MPI_COMM_WORLD);
+   }
+   
+   if(rank == 0)
+   { int totalsum = localsum;
+     cout<<"Localsum on processor 0 "<<localsum<<endl;
+     int sum1;
+     for(int i=1;i<size;i++)
+     {
+     MPI_Recv(&sum1,1,MPI_INT,i,2,MPI_COMM_WORLD,status);
+     totalsum += sum1;
+     }
+   cout<<"Totalsum ="<<totalsum<<endl;
+   }
    MPI_Finalize();
  }
